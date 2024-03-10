b9fdb6cb9118d39b853f30142f75b9e18ca1d667
Mazen Fayez <mazenfayez56@gmail.com>
2024-03-10 17:56:39 +0200

    first commit

---
+++ src/admin/app.example.js
@@ -0,0 +1,19 @@
+const config = {
+  translate:{
+    en:{
+      "app.title": "My App",
+      "app.description": "My App Description",
+    },
+    }
+  }
+  
+
+
+const bootstrap = (app) => {
+  console.log(app);
+};
+
+export default {
+  config,
+  bootstrap,
+};
